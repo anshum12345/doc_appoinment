4303358af50003fa4db57d28813cb72ce441ca83
anshum12345 <anshumdwivedi8@gmail.com>
2025-03-07 01:06:38 +0530

    AdminUpdate

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -1,16 +1,57 @@
 import { createContext, useState } from "react";
-
+import axios from 'axios';
+import { toast } from 'react-toastify';
 export const AdminContext = createContext();
 
 const AdminContextProvider = (props) => {
+
   const [aToken, setAToken] = useState(localStorage.getItem('aToken') ? localStorage.getItem('aToken') : '');
 
+ const [doctors, setDoctors] = useState([]);
+
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+  const getAllDoctors = async () => {
+    try{
+     const {data} = await axios.post(backendUrl + '/api/admin/all-doctors', {}, {headers: {aToken}})
+      if(data.success){
+        setDoctors(data.doctors)
+        console.log(data.doctors)
+      }
+      else{
+        toast.error(data.message);
+      }
+    }
+    catch(error){
+      toast.error(error.message);
+    }
+  }
+
+
+  // for change availability
+
+const changeAvailablity = async (docId) => {
+  try {
+    const { data } = await axios.post(backendUrl + '/api/doctor/change-availablity', { docId }, { headers: { aToken } })
+    if (data.success) {
+      toast.success(data.message)
+      getAllDoctors()
+    }
+     else {
+      toast.error(data.message)
+     }
+  } catch (error) {
+    toast.error(error.message);
+  }
+}
 
   const value = {
     aToken, // Corrected the typo here
     setAToken,
     backendUrl,
+    doctors,
+    getAllDoctors,
+    changeAvailablity
   };
 
   return (
--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -80,5 +80,15 @@ const loginAdmin = async (req, res) => {
   }
 }
 
+// API to get alll doctors list for admin pannel
+const allDoctors = async (req, res) => {
+  try {
+    const doctors = await doctorModel.find({}).select('-password')
+    res.json({ success: true, doctors })
+  } catch (error) {
+    console.log(error)
+    res.json({ success: false, message: error.message })
+  }
+}
 
-export {addDoctor,loginAdmin}
+export {addDoctor,loginAdmin,allDoctors}
