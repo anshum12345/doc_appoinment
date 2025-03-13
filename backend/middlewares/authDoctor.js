a5cc22abbc75060fdca5e24a826381fa94820b8a
anshum12345 <anshumdwivedi8@gmail.com>
2025-03-14 01:03:33 +0530

    envupdate

--- backend/middlewares/authDoctor.js
+++ backend/middlewares/authDoctor.js
@@ -1,25 +1,25 @@
 import jwt from 'jsonwebtoken'
-
-// doc authentication middleware
-const authDoctor = async (req, res, next) => {
-
-  try {
-
-    const { doken } = req.headers
-    if (!doken) {
-      return res.json({ success: false, message: 'Not Authorized Login Again' })
-    }
-
-    const token_decode = jwt.verify(doken, process.env.JWT_SECRET)
-    req.body.userId = token_decode.id
-
-    next()
-
-  } catch (error) {
-    console.log(error)
-    res.json({ success: false, message: error.message })
-  }
-
-}
-
-export default authDoctor
+ 
+ // doc authentication middleware
+ const authDoctor = async (req, res, next) => {
+ 
+   try {
+ 
+     const { doken } = req.headers
+     if (!doken) {
+       return res.json({ success: false, message: 'Not Authorized Login Again' })
+     }
+ 
+     const token_decode = jwt.verify(doken, process.env.JWT_SECRET)
+     req.body.userId = token_decode.id
+ 
+     next()
+ 
+   } catch (error) {
+     console.log(error)
+     res.json({ success: false, message: error.message })
+   }
+ 
+ }
+ 
+ export default authDoctor
--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,98 +1,85 @@
 import { createContext, useEffect, useState } from "react";
-import axios from "axios";
-import { toast } from "react-toastify";
-
-export const AppContext = createContext();
-
-const AppContextProvider = ({ children }) => {
-  const currencySymbol = "$";
-  const backendUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:4000"; // Fallback URL
-  const [doctors, setDoctors] = useState([]);
-  const [token, setToken] = useState(localStorage.getItem("token") || null);
-  const[userData, setUserData] = useState(false);
-
-
-  const getDoctorsData = async () => {
-    if (!backendUrl) {
-      console.error("Backend URL is missing.");
-      return;
-    }
-
-    try {
-      const { data } = await axios.get(`${backendUrl}/api/doctor/list`);
-      if (data.success) {
-        setDoctors(data.doctors);
-      } else {
-        toast.error(data.message);
-      }
-    } catch (error) {
-      console.error("Error fetching doctors:", error);
-      toast.error(error.response?.data?.message || "Failed to fetch doctors.");
-    }
-  };
-
-
-
-  const loadUserProfileData = async () => {
-
-    try {
-
-      const { data } = await axios.get(backendUrl + '/api/user/get-profile', { headers: { token } })
-
-      if (data.success) {
-        setUserData(data.userData)
-      } else {
-        toast.error(data.message)
-      }
-
-    } catch (error) {
-      console.log(error)
-      toast.error(error.message)
-    }
-
-  }
-
-
-  useEffect(() => {
-    getDoctorsData();
-  }, [backendUrl]); // Runs when backendUrl changes
-
-  useEffect(() => {
-    if (token) {
-      loadUserProfileData()
-    } else {
-      setUserData(false)
-    }
-  }, [token])
-
-
-  const value = {
-    doctors, getDoctorsData,
-    currencySymbol,
-    token,
-    setToken,
-    backendUrl, 
-    loadUserProfileData,
-    userData,
-    setUserData,
-  };
-
-  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
-};
-
-export default AppContextProvider;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ import axios from "axios";
+ import { toast } from "react-toastify";
+ 
+ export const AppContext = createContext();
+ 
+ const AppContextProvider = ({ children }) => {
+  //  const currencySymbol = "₹";
+   const currencySymbol = "$";
+   const backendUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:4000"; // Fallback URL
+   const [doctors, setDoctors] = useState([]);
+   const [token, setToken] = useState(localStorage.getItem("token") || null);
+   const[userData, setUserData] = useState(false);
+ 
+ 
+   const getDoctorsData = async () => {
+     if (!backendUrl) {
+       console.error("Backend URL is missing.");
+       return;
+     }
+ 
+     try {
+       const { data } = await axios.get(`${backendUrl}/api/doctor/list`);
+       if (data.success) {
+         setDoctors(data.doctors);
+       } else {
+         toast.error(data.message);
+       }
+     } catch (error) {
+       console.error("Error fetching doctors:", error);
+       toast.error(error.response?.data?.message || "Failed to fetch doctors.");
+     }
+   };
+ 
+ 
+ 
+   const loadUserProfileData = async () => {
+ 
+     try {
+ 
+       const { data } = await axios.get(backendUrl + '/api/user/get-profile', { headers: { token } })
+ 
+       if (data.success) {
+         setUserData(data.userData)
+       } else {
+         toast.error(data.message)
+       }
+ 
+     } catch (error) {
+       console.log(error)
+       toast.error(error.message)
+     }
+ 
+   }
+ 
+ 
+   useEffect(() => {
+     getDoctorsData();
+   }, [backendUrl]); // Runs when backendUrl changes
+ 
+   useEffect(() => {
+     if (token) {
+       loadUserProfileData()
+     } else {
+       setUserData(false)
+     }
+   }, [token])
+ 
+ 
+   const value = {
+     doctors, getDoctorsData,
+     currencySymbol,
+     token,
+     setToken,
+     backendUrl, 
+     loadUserProfileData,
+     userData,
+     setUserData,
+   };
+ 
+   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+ };
+ 
+ export default AppContextProvider;
+ 
